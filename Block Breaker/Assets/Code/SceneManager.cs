c828534e8d023f76f867ebba36382021068ff21f
Natasha Martinez <nem4265@g.rit.edu>
2018-01-09 19:49:16 -0500

    Section 5, Lecture 63

---
+++ Block Breaker/Assets/Code/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
