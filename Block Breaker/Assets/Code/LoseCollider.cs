85871a51c5725082127fb768e70175dac2933a7e
Natasha Martinez <nem4265@g.rit.edu>
2018-01-16 13:00:58 -0500

    S5 L78

---
+++ Block Breaker/Assets/Code/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	public SceneManager levelManager;
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		print ("trigger");
+		levelManager.LoadLevel("WinScreen");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision2D) {
+		print ("collision");
+	}
+}
---
+++ Block Breaker/Assets/Code/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouseTransform = new Vector3(0.5f, this.transform.position.y, this.transform.position.z);
+
+ 		float paddlePos = Input.mousePosition.x / Screen.width * 16;
+
+ 		mouseTransform.x = Mathf.Clamp(paddlePos, 0.5f, 15.5f);
+		print(mouseTransform.x);
+
+		this.transform.position = mouseTransform;
+	}
+}
